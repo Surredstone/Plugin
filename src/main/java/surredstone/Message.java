ca2f8842973aaabaa066330d65153b775995061a
Panem <titoluccao@gmail.com>
2022-05-28 19:36:30 -0300

    feat: add Message PLAYER_WITHOUT_VILLAGE

--- src/main/java/surredstone/Message.java
+++ src/main/java/surredstone/Message.java
@@ -7,4 +7,5 @@ public interface Message {
     public String ON_PLUGIN_DISABLE = ChatColor.RED + "Surredstone Plugin disabled";
     public String INVALID_VILLAGE_STORAGE_ERROR = "Invalid Village Storage";
     public String COMMAND_MUST_BE_EXECUTED_BY_PLAYER = "This command must be executed by a player";
+    public String PLAYER_WITHOUT_VILLAGE = "To run this command you must be in a Village";
 }
