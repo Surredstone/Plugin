7f0e84e1121c13f251abe881ad8c1c5b9e097a20
Panem <titoluccao@gmail.com>
2022-05-29 16:16:34 -0300

    refatore: remove permission handling from Village

--- src/main/java/surredstone/commands/VillageCommand.java
+++ src/main/java/surredstone/commands/VillageCommand.java
@@ -10,11 +10,6 @@ import surredstone.Village;
 public class VillageCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!sender.hasPermission("surredstone.village")) {
-            sender.sendMessage(Message.PERMISSIONS_INSUFICIENTS);
-            return false;
-        }
-
         Village requestedVillage = Village.getVillageByAbbreviation(args[0].toString());
 
         if (requestedVillage == null) {
