de3c7d5cc336db8e60a70019a02a74e3c3133983
Panem <titoluccao@gmail.com>
2022-05-30 16:12:51 -0300

    feat: add sendGlobalMessage

--- src/main/java/surredstone/Message.java
+++ src/main/java/surredstone/Message.java
@@ -11,4 +11,40 @@ public interface Message {
     public String PERMISSIONS_INSUFICIENTS = "Permissões insuficientes";
     public String VILLAGE_NOT_FOUND = "Vila não encontrada";
     public String SUBCOMMAND_INVALID = "Subcomando inválido";
+    public String DISCORD = ChatColor.AQUA + "[DC]";
+    public String GLOBAL = ChatColor.GOLD + "Global";
+
+    static String sendGlobalMessage(String userName, String message, boolean fromDiscord) {
+        String prefix = "";
+
+        if (fromDiscord) {
+            prefix += String.format(
+                    "%s %s",
+                    DISCORD,
+                    GLOBAL);
+        } else {
+            Village playerVillage = Village.getVillageByPlayer(Plugin.getInstance().getServer().getPlayer(userName));
+
+            if (playerVillage == null)
+                return null;
+
+            prefix += String.format(
+                    "%s %s[%s]",
+                    GLOBAL,
+                    playerVillage.getTextColor(),
+                    playerVillage.getName());
+        }
+
+        String finalMessage = String.format(
+                "%s %s%s: %s",
+                prefix,
+                ChatColor.WHITE,
+                userName,
+                message);
+
+        Plugin.getInstance().getServer().getConsoleSender().sendMessage(finalMessage);
+        Plugin.getInstance().getServer().broadcastMessage(finalMessage);
+
+        return finalMessage;
+    }
 }
