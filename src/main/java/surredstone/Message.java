e0fca4a24bdc4beab1f1e800aca268b78749edad
Panem <titoluccao@gmail.com>
2022-05-30 15:26:39 -0300

    feat: translate Messages to portuguese

--- src/main/java/surredstone/Message.java
+++ src/main/java/surredstone/Message.java
@@ -3,12 +3,12 @@ package surredstone;
 import org.bukkit.ChatColor;
 
 public interface Message {
-    public String ON_PLUGIN_ENABLE = ChatColor.GREEN + "Surredstone Plugin enabled";
-    public String ON_PLUGIN_DISABLE = ChatColor.RED + "Surredstone Plugin disabled";
-    public String INVALID_VILLAGE_STORAGE_ERROR = "Invalid Village Storage";
-    public String COMMAND_MUST_BE_EXECUTED_BY_PLAYER = "This command must be executed by a player";
-    public String PLAYER_WITHOUT_VILLAGE = "To run this command you must be in a Village";
-    public String PERMISSIONS_INSUFICIENTS = "Permissions insuficients";
-    public String VILLAGE_NOT_FOUND = "Village not found";
-    public String SUBCOMMAND_INVALID = "Subcommand invalid";
+    public String ON_PLUGIN_ENABLE = ChatColor.GREEN + "Plugin Surredstone habilitado";
+    public String ON_PLUGIN_DISABLE = ChatColor.RED + "Plugin Surredstone desabilitado";
+    public String INVALID_VILLAGE_STORAGE_ERROR = "Armazenamento da Vila inválido";
+    public String COMMAND_MUST_BE_EXECUTED_BY_PLAYER = "Este comando deve ser executado por um player";
+    public String PLAYER_WITHOUT_VILLAGE = "Você deve estar em uma vila para executar esse comando";
+    public String PERMISSIONS_INSUFICIENTS = "Permissões insuficientes";
+    public String VILLAGE_NOT_FOUND = "Vila não encontrada";
+    public String SUBCOMMAND_INVALID = "Subcomando inválido";
 }
