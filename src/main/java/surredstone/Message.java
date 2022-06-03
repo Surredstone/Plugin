7824036e46a34548c07d1579f9d6d335ed3293f7
Panem <titoluccao@gmail.com>
2022-06-03 16:51:26 -0300

    fix(Message): fix unicode characters

--- src/main/java/surredstone/Message.java
+++ src/main/java/surredstone/Message.java
@@ -9,22 +9,22 @@ import org.bukkit.entity.Player;
 public class Message {
     public static String ON_PLUGIN_ENABLE = ChatColor.GREEN + "Plugin Surredstone habilitado";
     public static String ON_PLUGIN_DISABLE = ChatColor.RED + "Plugin Surredstone desabilitado";
-    public static String INVALID_VILLAGE_STORAGE_ERROR = ChatColor.RED + "Armazenamento da Vila inválido";
+    public static String INVALID_VILLAGE_STORAGE_ERROR = ChatColor.RED + "Armazenamento da Vila inv\u00E1lido";
     public static String COMMAND_MUST_BE_EXECUTED_BY_PLAYER = ChatColor.RED
             + "Este comando deve ser executado por um player";
     public static String PLAYER_WITHOUT_VILLAGE = ChatColor.RED
-            + "Você deve estar em uma vila para executar esse comando";
-    public static String PERMISSIONS_INSUFICIENTS = ChatColor.RED + "Permissões insuficientes";
-    public static String VILLAGE_NOT_FOUND = ChatColor.RED + "Vila não encontrada";
-    public static String SUBCOMMAND_INVALID = ChatColor.RED + "Subcomando inválido";
+            + "Voc\u00EA deve estar em uma vila para executar esse comando";
+    public static String PERMISSIONS_INSUFICIENTS = ChatColor.RED + "Permiss\u00F5es insuficientes";
+    public static String VILLAGE_NOT_FOUND = ChatColor.RED + "Vila n\u00E3o encontrada";
+    public static String SUBCOMMAND_INVALID = ChatColor.RED + "Subcomando inv\u00E1lido";
     public static String DISCORD = ChatColor.AQUA + "[DC]";
     public static String GLOBAL = ChatColor.GOLD + "Global";
     public static String VILLAGE = ChatColor.GOLD + "Vila";
     public static String BOT_LOGIN_FAILURE = ChatColor.RED + "Falha ao conectar ao Discord";
     public static String BOT_LOGIN_SUCCESS = ChatColor.GREEN + "Bot connectado como %s";
-    public static String INFO_INFO = ChatColor.AQUA + "Informações: %s";
+    public static String INFO_INFO = ChatColor.AQUA + "Informa\u00E7\u00F5es: %s";
     public static String INFO_MEMBERS = ChatColor.AQUA + "Membros online: %s";
-    public static String PARAM_INSUFICIENTS = ChatColor.RED + "Parâmetros insuficientes";
+    public static String PARAM_INSUFICIENTS = ChatColor.RED + "Par\u00E2metros insuficientes";
 
     private String name;
     private String message;
