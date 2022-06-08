7a5cef79cc6d4c9ea09ba97fe70adbfe2f147fef
Panem <titoluccao@gmail.com>
2022-06-08 20:02:29 -0300

    feat(Log): remove unused logs

--- src/main/java/surredstone/DiscordLog.java
+++ src/main/java/surredstone/DiscordLog.java
@@ -1,7 +1,6 @@
 package surredstone;
 
 public enum DiscordLog {
-    PLAYER_ADVANCEMENT(":emote:", "%player conseguiu a conquista %advancement"),
     PLAYER_DEATH(":emote:", "%death_message"),
     PLAYER_JOIN(":emote:", "%player entrou no servidor"),
     PLAYER_QUIT(":emote:", "%player saiu do servidor"),
